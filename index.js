f9dc71603099cac67f69277ff73d7c7c375a5163
svillahermosa <santiagovillahermosa@gmail.com>
2025-02-24 13:55:26 -0400

    what happened 1

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const app = express()
 
 const corsOptions = {
     origin: process.env.FRONT_URL,
+    credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
 
